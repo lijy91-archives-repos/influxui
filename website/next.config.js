9989314ad0b0504f905e468dfe7dfeaad1739533
李健莹 <lijy91@foxmail.com>
2023-07-16 23:14:30 +0800

    chore: Update website

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
