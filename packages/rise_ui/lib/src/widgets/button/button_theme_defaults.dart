35ff556772e37af13675a40d8907925ae40ed0ec
李健莹 <lijy91@foxmail.com>
2023-12-29 22:42:46 +0800

    wip: Update button theme defaults

--- packages/rise_ui/lib/src/widgets/button/button_theme_defaults.dart
+++ packages/rise_ui/lib/src/widgets/button/button_theme_defaults.dart
@@ -6,47 +6,47 @@ const _defaults = ButtonThemeData(
   borderRadius: BorderRadius.all(Radius.circular(4)),
   pressedOpacity: 0.8,
   tinyStyle: ButtonStyle(
-    padding: EdgeInsets.symmetric(horizontal: 14),
-    minimumSize: Size.square(30),
+    padding: EdgeInsets.symmetric(horizontal: 6),
+    minimumSize: Size.square(18),
     iconSize: 12,
+    labelStyle: TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w600,
+    ),
+  ),
+  smallStyle: ButtonStyle(
+    padding: EdgeInsets.symmetric(horizontal: 8),
+    minimumSize: Size.square(22),
+    iconSize: 16,
     labelStyle: TextStyle(
       fontSize: 12,
       fontWeight: FontWeight.w600,
     ),
   ),
-  smallStyle: ButtonStyle(
-    padding: EdgeInsets.symmetric(horizontal: 18),
-    minimumSize: Size.square(36),
-    iconSize: 16,
+  mediumStyle: ButtonStyle(
+    padding: EdgeInsets.symmetric(horizontal: 10),
+    minimumSize: Size.square(28),
+    iconSize: 20,
     labelStyle: TextStyle(
       fontSize: 14,
       fontWeight: FontWeight.w600,
     ),
   ),
-  mediumStyle: ButtonStyle(
-    padding: EdgeInsets.symmetric(horizontal: 22),
-    minimumSize: Size.square(42),
-    iconSize: 20,
+  largeStyle: ButtonStyle(
+    padding: EdgeInsets.symmetric(horizontal: 12),
+    minimumSize: Size.square(34),
+    iconSize: 24,
     labelStyle: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w600,
     ),
   ),
-  largeStyle: ButtonStyle(
-    padding: EdgeInsets.symmetric(horizontal: 26),
-    minimumSize: Size.square(50),
-    iconSize: 24,
-    labelStyle: TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w600,
-    ),
-  ),
   bigStyle: ButtonStyle(
-    padding: EdgeInsets.symmetric(horizontal: 32),
-    minimumSize: Size.square(60),
+    padding: EdgeInsets.symmetric(horizontal: 14),
+    minimumSize: Size.square(44),
     iconSize: 32,
     labelStyle: TextStyle(
-      fontSize: 20,
+      fontSize: 18,
       fontWeight: FontWeight.w600,
     ),
   ),
