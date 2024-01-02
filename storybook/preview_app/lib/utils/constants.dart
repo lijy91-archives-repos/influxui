6f073a1c360038b36f3867ff67f56a8e3e4caf21
李健莹 <lijy91@foxmail.com>
2024-01-02 23:07:29 +0800

    wip: Update storybook preview_app

---
+++ storybook/preview_app/lib/utils/constants.dart
@@ -0,0 +1,24 @@
+import 'package:rise_ui/rise_ui.dart';
+
+final kAllColors = [
+  ExtendedColors.gray,
+  ExtendedColors.red,
+  ExtendedColors.pink,
+  ExtendedColors.grape,
+  ExtendedColors.violet,
+  ExtendedColors.indigo,
+  ExtendedColors.blue,
+  ExtendedColors.cyan,
+  ExtendedColors.teal,
+  ExtendedColors.green,
+  ExtendedColors.lime,
+  ExtendedColors.yellow,
+];
+
+final kNamedSizes = [
+  NamedSize.tiny,
+  NamedSize.small,
+  NamedSize.medium,
+  NamedSize.large,
+  NamedSize.big,
+];
