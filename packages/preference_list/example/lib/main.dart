392e37a6896631f37a88d9fc61f6807d0a38a879
李健莹 <lijy91@foxmail.com>
2024-05-19 21:11:45 +0800

    chore(preference_list): Add example

---
+++ packages/preference_list/example/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:preference_list/preference_list.dart';
+import 'package:flutter/cupertino.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Plugin example app'),
+//         ),
+//         body: Center(
+//           child: Text(''),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+/// Flutter code sample for base [CupertinoListSection] and [CupertinoListTile].
+
+void main() => runApp(const CupertinoListSectionBaseApp());
+
+class CupertinoListSectionBaseApp extends StatelessWidget {
+  const CupertinoListSectionBaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: ListSectionBaseExample(),
+    );
+  }
+}
+
+class ListSectionBaseExample extends StatelessWidget {
+  const ListSectionBaseExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: CupertinoListSection(
+        header: const Text('My Reminders'),
+        children: <CupertinoListTile>[
+          CupertinoListTile(
+            title: const Text('Open pull request'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.activeGreen,
+            ),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+          CupertinoListTile(
+            title: const Text('Push to master'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.systemRed,
+            ),
+            additionalInfo: const Text('Not available'),
+          ),
+          CupertinoListTile(
+            title: const Text('View last commit'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.activeOrange,
+            ),
+            additionalInfo: const Text('12 days ago'),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Last commit');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SecondPage extends StatelessWidget {
+  const _SecondPage({required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
